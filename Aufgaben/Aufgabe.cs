e884e6f0ff2fd699467aa6d8c7ab549e42349691
Helexyastis <latzi_94@yahoo.de>
2017-07-14 07:56:43 +0200

    changes

--- Aufgaben/Aufgabe.cs
+++ Aufgaben/Aufgabe.cs
@@ -9,7 +9,7 @@ namespace Aufgaben
     class Aufgabe
     {
         enum ZeitStatus { red,green,yellow};
-        List<Aufgabe> subAufgaben;
+        //List<Aufgabe> subAufgaben;
         Aufgabe parent;
         string kontakt;
         string status;
@@ -23,10 +23,16 @@ namespace Aufgaben
         string name;
         int id;
         public Aufgabe Partent { get { return parent; } set { parent = value; } }
+        public List<Aufgabe> ChildTasks { get; set; }
         public string Kontakt { get { return kontakt; } set { kontakt = value; } }
         public string Status { get { return status; } set { status = value; } }
         public TimeSpan ZeitAufwand {get { return zeitAufwand; } set { zeitAufwand = value; } } 
         public TimeSpan TimeLeft { get { return abgabe.Subtract(DateTime.Now); }}
+        public DateTime AnnahmeDatum { get { return annahme; }set { annahme = value; } }
+        public DateTime AbgabeDatum { get { return abgabe; } set { abgabe = value; } }
+        public string Auftragsnumemr { get { return aufnr; }set { aufnr = value; } }
+        public int ID { get { return id; } set { id = value; } }
+
         public Aufgabe(int id,string name)
         {
             this.name = name;
@@ -35,7 +41,7 @@ namespace Aufgaben
 
         public Aufgabe(string name,Aufgabe parent,string kontakt,string status,DateTime annahme,DateTime abgabe,string aufnr)
         {
-            subAufgaben = new List<Aufgabe>();
+            ChildTasks = new List<Aufgabe>();
             this.name = name;
             this.parent = parent;
             this.kontakt = kontakt;
@@ -47,7 +53,7 @@ namespace Aufgaben
             zeitStatus = ZeitStatus.green;
             if(parent != null)
             {
-                parent.subAufgaben.Add(this);
+                parent.ChildTasks.Add(this);
             }
         }
         private void SetZeitStatus()
--- Aufgaben/AufgabenManager.cs
+++ Aufgaben/AufgabenManager.cs
@@ -12,7 +12,6 @@ namespace Aufgaben
         List<Aufgabe> aufgaben;
         string pathToXML = @"C:\DTS\Projects\Aufgaben\Aufgaben\Aufgaben.xml";
         XmlDocument document;
-
         public AufgabenManager()
         {
             aufgaben = new List<Aufgabe>();
@@ -31,14 +30,44 @@ namespace Aufgaben
             {
                 string name = task.Attributes[1].Value;
                 int id = Convert.ToInt32(task.Attributes[0].Value);
-                
+                Aufgabe aufgabe = new Aufgabe(id, name);
                 XmlNodeList values = task.ChildNodes;
                 foreach(XmlNode value in values)
                 {
-                    if(value.Name == "annahme") 
+                    switch (value.Name)
+                    {
+                        case "annahme":
+                            aufgabe.AnnahmeDatum = Convert.ToDateTime(value.Value);
+                            break;
+                        case "abgabe":
+                            break;
+                        case "parent":
+                            break;
+                        case "konakt":
+                            break;
+                        case "status":
+                            break;
+                        case "zeitaufwand":
+                            break;
+                        case "subtasks":
+                            break;
+
+                        default:
+                            break;
+                    }
+                    
                 }
-                Aufgabe aufgabe;
+               
             }
+        }
+
+        private void SaveNewTasks(Aufgabe task)
+        {
+            XmlNode root = document.SelectSingleNode("aufgaben");
+            XmlNode nodeToAdd = document.CreateElement("aufgabe");
+
+            root.AppendChild(nodeToAdd);
+
         }
     }
 }
