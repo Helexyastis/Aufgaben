0a92ca91194f4247b2c43fae8753b9ecb5bcc827
Helexyastis <Helexyastis@users.noreply.github.com>
2017-07-21 19:20:59 +0200

    start control

---
+++ Aufgaben/AufgabenControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aufgaben
+{
+    class AufgabenControl : Control
+    {
+
+        Label lName;
+        
+        public AufgabenControl() 
+            : base()
+        {
+
+        }
+
+        public AufgabenControl(string text, int left, int top, int width, int height) 
+            : base(text, left, top, width, height)
+        {
+
+        }
+
+       
+    }
+}
